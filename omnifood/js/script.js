fbe2d4f31c4693807b0c8f7206c0f4565f60f704
Aleksandr Dus <aleksandr.dus@scompler.com>
2021-11-30 23:53:38 +0300

    Add smooth scroll and make links work

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -9,4 +9,27 @@
   btnNavEl.addEventListener('click', () => {
     headerEl.classList.toggle('nav-open')
   })
+
+  const allLinks = document.querySelectorAll('a:link')
+  allLinks.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault()
+      const href = link.getAttribute('href')
+
+      if (href === '#') {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+
+        return
+      }
+
+      if (!href.startsWith('#')) return
+
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
+
+      if (link.classList.contains('main-nav-link')) {
+        headerEl.classList.toggle('nav-open')
+      }
+    })
+  })
 })()
