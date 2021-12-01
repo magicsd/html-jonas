e19e2a06646dbb42b053ec1bce2285f321385d9e
Aleksandr Dus <aleksandr.dus@scompler.com>
2021-12-01 19:01:15 +0300

    Add sticky header

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -1,35 +1,52 @@
-(() => {
-  const yearEl = document.getElementById('year')
+;(() => {
+  const yearEl = document.getElementById("year")
   const currentYear = new Date().getFullYear()
   yearEl.textContent = String(currentYear)
 
-  const btnNavEl = document.querySelector('.btn-mobile-nav')
-  const headerEl = document.querySelector('.header')
+  const btnNavEl = document.querySelector(".btn-mobile-nav")
+  const headerEl = document.querySelector(".header")
+  const bodyEl = document.body
 
-  btnNavEl.addEventListener('click', () => {
-    headerEl.classList.toggle('nav-open')
+  btnNavEl.addEventListener("click", () => {
+    headerEl.classList.toggle("nav-open")
   })
 
-  const allLinks = document.querySelectorAll('a:link')
+  const allLinks = document.querySelectorAll("a:link")
   allLinks.forEach(link => {
-    link.addEventListener('click', event => {
+    link.addEventListener("click", event => {
       event.preventDefault()
-      const href = link.getAttribute('href')
+      const href = link.getAttribute("href")
 
-      if (href === '#') {
-        window.scrollTo({ top: 0, behavior: 'smooth' })
+      if (href === "#") {
+        window.scrollTo({ top: 0, behavior: "smooth" })
 
         return
       }
 
-      if (!href.startsWith('#')) return
+      if (!href.startsWith("#")) return
 
       const sectionEl = document.querySelector(href)
-      sectionEl.scrollIntoView({ behavior: 'smooth' })
+      sectionEl.scrollIntoView({ behavior: "smooth" })
 
-      if (link.classList.contains('main-nav-link')) {
-        headerEl.classList.toggle('nav-open')
+      if (link.classList.contains("main-nav-link")) {
+        headerEl.classList.toggle("nav-open")
       }
     })
   })
+
+  const observer = new IntersectionObserver(entries => {
+    const [entry] = entries
+
+    if (entry.isIntersecting) {
+      bodyEl.classList.remove('sticky')
+    } else {
+      bodyEl.classList.add('sticky')
+    }
+  }, {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  })
+
+  observer.observe(document.querySelector(".section-hero"))
 })()
